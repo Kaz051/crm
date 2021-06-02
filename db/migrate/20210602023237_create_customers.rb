3f279ff530e85c84cfc4364290147f2953663766
Kaz051 <kznlion@gmail.com>
2021-06-02 12:20:44 +0900

    customersテーブル作成

--- db/migrate/20210602023237_create_customers.rb
+++ db/migrate/20210602023237_create_customers.rb
@@ -1,7 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
-
+      t.references :user, type: :bigint, null:false, foreign_key: true
+      t.string     :name,                null: false
       t.timestamps
     end
   end
