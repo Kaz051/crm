8c880bdad3c1ba5d8026b1ef994a94ed234b328b
Kaz051 <kznlion@gmail.com>
2021-06-02 14:46:26 +0900

    customersテーブル編集

--- db/migrate/20210602023237_create_customers.rb
+++ db/migrate/20210602023237_create_customers.rb
@@ -1,7 +1,6 @@
 class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
-      t.references :user, type: :bigint, null:false, foreign_key: true
       t.string     :name,                null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_06_02_023237) do
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_customers_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -33,5 +31,4 @@ ActiveRecord::Schema.define(version: 2021_06_02_023237) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "customers", "users"
 end
