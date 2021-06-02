2b9d4611d330c3e8bcbc81e56dbbbe033e2da399
Kaz051 <kznlion@gmail.com>
2021-06-02 18:43:21 +0900

    customersコントローラーの編集

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,28 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Cusomer.all
+    @customers = Customer.all.order('created_at DESC')
   end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name)
+  end
+
 end
