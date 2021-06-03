291c115e8a0e64ee38f79908ce417199e2388bca
Kaz051 <kznlion@gmail.com>
2021-06-03 15:08:07 +0900

    Itemモデル単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品情報の登録' do
+    context '商品情報を登録できるとき' do
+      it 'nameとpurchase_priceが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品情報を登録できないとき' do
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'purchase_priceが空では登録できない' do
+        @item.purchase_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Purchase price can't be blank")
+      end
+    end  
+  end
+end  
