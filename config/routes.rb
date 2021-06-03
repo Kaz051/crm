63204eea2c48111cc8ecd123ad1e166f839a23bd
Kaz051 <kznlion@gmail.com>
2021-06-03 16:38:15 +0900

    usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: "customers#index"
   resources :customers, only: [:index, :new, :create, :show]
   resources :items, only: [:index, :new, :create]
+  resources :users, only: :show
 end
