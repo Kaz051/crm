51b104b3594f45bba35563783a8734760291e50f
Kaz051 <kznlion@gmail.com>
2021-06-08 20:53:32 +0900

    postsコントローラー作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def create
+    post = Post.create(post_params)
+    redirect_to "customers#index"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text).merge(user_id: current_user.id)
+  end
+end
