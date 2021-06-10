3ed2ac33de0cc8c3007b9af95f70d2a957f3699d
Kaz051 <kznlion@gmail.com>
2021-06-10 20:07:33 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "customers#index"
-  resources :customers, only: [:index, :new, :create, :show, :destroy] do
+  resources :customers, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
     resources :estimates, only: [:create, :destroy]
     resource :bookmarks, only: [:create, :destroy]
   end  
