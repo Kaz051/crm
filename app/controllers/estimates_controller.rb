06de262f82bc23ff9e27a95305ca723d5277b335
Kaz051 <kznlion@gmail.com>
2021-06-10 16:50:36 +0900

    見積り削除機能

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -8,6 +8,12 @@ class EstimatesController < ApplicationController
     end
   end
 
+  def destroy
+    @estimate = Estimate.find(params[:customer_id])
+    @estimate.destroy
+    redirect_to customer_path(@estimate.customer.id)
+  end
+
   private
   def estimate_params
     params.require(:estimate).permit(:item_name, :selling_price).merge(user_id: current_user.id, customer_id: params[:customer_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "customers#index"
   resources :customers, only: [:index, :new, :create, :show, :destroy] do
-    resources :estimates, only: :create
+    resources :estimates, only: [:create, :destroy]
     resource :bookmarks, only: [:create, :destroy]
   end  
   resources :items, only: [:index, :new, :create, :destroy]
